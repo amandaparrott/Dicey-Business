ab208ede0499f5c45a3137db60bccd87f39beb81
Amanda Parrott <amandaparrott@amandas-air.attlocal.net>
2020-10-06 15:15:52 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+
+class Die {
+    constructor (value) {
+        this.value = value;
+    }        
+    roll () {
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //end of body
+});
